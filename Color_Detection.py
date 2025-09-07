97d56c7b7c73806ab3035156e92dff59ddc5a358
Mansi Sharma <mansimsharma11@gmail.com>
2025-09-07 20:02:24 +0530

    Update Color_Detection.py

--- Color_Detection.py
+++ Color_Detection.py
@@ -1,5 +1,5 @@
 import cv2
-from utility import get_limits 
+from util import get_limits 
 from PIL import Image
 
 yellow = [0, 255, 255]
@@ -30,4 +30,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
 
